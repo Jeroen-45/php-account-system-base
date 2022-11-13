1b5eb0d60022af9be7cac32d7737c5fe84c3ff15
Jeroen Tas <jeroentas@live.nl>
2022-11-13 01:33:43 +0100

    Absolute basic index.php with header and footer

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$title = "Base account system";
+include "includes/header.php";
+?>
+<h1>Hello world!</h1>
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+<?php
+include "includes/footer.php";
+?>
